92882c4ff5d5d7656068367b7ddd3e879b10f697
Ryan Asher <ryandeanasher@gmail.com>
2016-03-24 23:21:41 -0400

    Update deployment script and minor padding tweak.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## My Website
 
 This repo contains the really basic HTML and CSS I use to power my personal webpage, and some of my static projects.
+
+### Deploying
+
+When I make changes to the HTML or the static files, I can run the following command to sync the project directory with
+my s3 bucket:
+
+	$ ./deploy.sh [email]
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-aws s3 cp $1 s3://ryandasher.com/$3 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=emailaddress=$2
+aws s3 sync . s3://ryandasher.com \
+--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
+full=emailaddress=$1 --exclude="*.git/*" --exclude="README.md" \
+--exclude="LICENSE.txt" --exclude="deploy.sh"
